c4ca346fef518bc9fcc7d7ba04885305dc695e23
Nicollas Marinho <nicollasmarinhol@gmail.com>
2022-07-20 20:30:37 -0300

    categories and favorites

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_meals_screen.dart';
 import 'package:meals/screens/meal_detail_screen.dart';
+import 'package:meals/screens/settings_screen.dart';
 import 'package:meals/utils/app_routes.dart';
-import 'screens/categories_screens.dart';
 import 'screens/tabs_screen.dart';
 
 void main() => runApp(MyApp());
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
           AppRoutes.HOME: (ctx) => TabsScreen(),
           AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
           AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
+          AppRoutes.SETTINGS: (ctx) => SettingsScreen(),
         });
   }
 }
